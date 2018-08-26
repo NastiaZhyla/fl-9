d2444b6a892f445c7523d212dcf21e5dec65fcaa
Anastasiia Zhyla <41120397+NastiaZhyla@users.noreply.github.com>
2018-08-26 21:11:11 +0300

    Update isPrime.js

--- FE_9_8_homework_functions/homework/src/isPrime.js
+++ FE_9_8_homework_functions/homework/src/isPrime.js
@@ -2,7 +2,7 @@ function isPrime(number) {
   if (number < 2){
       return false;
   } 
-/*eslint no-magic-numbers: ["error", { "ignore": [0, e2, 5]}]*/   
+/*eslint no-magic-numbers: ["error", { "ignore": [0, 2, 5]}]*/   
   for (let i = 2; i < number; i++) {
     if (number % i === 0) {
       return false;
