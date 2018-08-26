35f25cc15106c219aec546d95027ec59d66bda5b
Anastasiia Zhyla <41120397+NastiaZhyla@users.noreply.github.com>
2018-08-26 21:19:57 +0300

    Update getClosestToZero.js

--- FE_9_8_homework_functions/homework/src/getClosestToZero.js
+++ FE_9_8_homework_functions/homework/src/getClosestToZero.js
@@ -1,7 +1,7 @@
-var getClosestToZero = [-3, -9, 8, 16, 2],
-  goal = 0;
-
-var getClosestToZero = getClosestToZero.reduce(function(prev, curr) {
-  return (Math.abs(curr - goal) < Math.abs(prev - goal) ? curr : prev);
+let getClosestToZero = [-3, -9, 8, 16, 2],
+goal = 0;
+/*eslint no-magic-numbers: ["error", { "ignore": [0, -3, -9, 8, 16, 2]}]*/  
+let result = getClosestToZero.reduce(function(prev, curr) {
+  return Math.abs(curr - goal) < Math.abs(prev - goal) ? curr : prev;
 });
-console.log(getClosestToZero);
+console.log(result);
