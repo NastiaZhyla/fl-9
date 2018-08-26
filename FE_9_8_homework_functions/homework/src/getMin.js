3732f3c9c78e947f041271b3a721a36cdf41f355
Anastasiia Zhyla <41120397+NastiaZhyla@users.noreply.github.com>
2018-08-26 21:31:57 +0300

    Update getMin.js

--- FE_9_8_homework_functions/homework/src/getMin.js
+++ FE_9_8_homework_functions/homework/src/getMin.js
@@ -1,7 +1,10 @@
 function getMin(a) {
-    var min = 0;
-    for (var i = 1; i < a.length; i++) {
-     if (a[i] < a[min]) min = i;
-    }
+    let min = 0;
+    for (let i = 1; i < a.length; i++) {
+     if (a[i] < a[min]){
+        min = i;
+     }
     return min;
    }
+}
+   console.log(getMin);
