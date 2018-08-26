c29f06888dfdd635af09e8c33a72cb7c7447aeca
Anastasiia Zhyla <41120397+NastiaZhyla@users.noreply.github.com>
2018-08-26 20:29:29 +0300

    Update getClosestToZero.js

--- FE_9_8_homework_functions/homework/src/getClosestToZero.js
+++ FE_9_8_homework_functions/homework/src/getClosestToZero.js
@@ -1,7 +1,7 @@
 var getClosestToZero = [-3, -9, 8, 16, 2],
   goal = 0;
 
-var getClosestToZero = counts.reduce(function(prev, curr) {
+var getClosestToZero = getClosestToZero.reduce(function(prev, curr) {
   return (Math.abs(curr - goal) < Math.abs(prev - goal) ? curr : prev);
 });
 console.log(getClosestToZero);
